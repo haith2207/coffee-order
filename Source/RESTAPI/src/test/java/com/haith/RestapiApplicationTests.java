1173832f33c7b54cb74c903074850ede0a522890
Hai Tran <haith2207@gmail.com>
2016-02-24 16:14:47 +0700

    convert project to spring boot with sqlserver

---
+++ Source/RESTAPI/src/test/java/com/haith/RestapiApplicationTests.java
@@ -0,0 +1,16 @@
+package com.haith;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = RestapiApplication.class)
+public class RestapiApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
